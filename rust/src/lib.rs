e4501615312591d02d8930ff1f7b162b6adac869
Xidorn Quan <me@upsuper.org>
2018-09-19 09:51:26 +1000

    Remove query_selector_infallible

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,6 @@
 mod expr;
-mod helpers;
 
 use crate::expr::{Expr, Unit};
-use crate::helpers::*;
 use lazy_static::lazy_static;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
@@ -58,7 +56,7 @@ fn add_event_listeners() -> Result<(), JsValue> {
         if target.id() == "add" {
             add_item()?;
         } else if target.class_name() == "remove" {
-            remove_item(target.closest("tr")?.unwrap());
+            remove_item(target.closest("tr")?.unwrap())?;
         }
         Ok(())
     })?;
@@ -89,21 +87,22 @@ fn add_item() -> Result<(), JsValue> {
     let new_record = DOC
         .import_node_with_deep(RECORD.content().as_ref(), true)?
         .unchecked_into::<DocumentFragment>();
-    let expr_elem = new_record.query_selector_infallible(".expr");
+    let expr_elem = new_record.query_selector(".expr")?.unwrap();
     Node::from(expr_elem).set_text_content(Some(&format!("{}", expr)));
-    let value_elem = new_record.query_selector_infallible(".value");
+    let value_elem = new_record.query_selector(".value")?.unwrap();
     Node::from(value_elem).set_text_content(Some(&format!("{} {}", value, UNIT)));
     RECORDS.insert_before(new_record.as_ref(), RECORDS.first_child().as_ref())?;
     update_total(value);
     Ok(())
 }
 
-fn remove_item(row: Element) {
-    let value_elem = row.query_selector_infallible(".value");
+fn remove_item(row: Element) -> Result<(), JsValue> {
+    let value_elem = row.query_selector(".value")?.unwrap();
     let value = Node::from(value_elem).text_content().unwrap();
     let value = Expr::parse(&value).unwrap().calc(UNIT);
     row.remove();
     update_total(-value);
+    Ok(())
 }
 
 fn update_total(diff: f32) {
