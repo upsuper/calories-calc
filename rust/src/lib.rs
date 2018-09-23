3541f2986fcedee488965cffdeeccf417ded645f
Xidorn Quan <me@upsuper.org>
2018-09-23 10:11:35 +1000

    Use unwrap_abort to save some codesize

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,5 @@
 mod expr;
 #[cfg(target_arch = "wasm32")]
 pub mod wasm;
+#[cfg(target_arch = "wasm32")]
+mod wasm_utils;
--- rust/src/wasm.rs
+++ rust/src/wasm.rs
@@ -1,4 +1,5 @@
 use crate::expr::{Expr, Unit};
+use crate::wasm_utils::UnwrapAbort;
 use lazy_static::lazy_static;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
@@ -10,8 +11,8 @@ use web_sys::{
 const UNIT: Unit = Unit::Kj;
 
 lazy_static! {
-    static ref WIN: Window = window().unwrap();
-    static ref DOC: Document = WIN.document().unwrap();
+    static ref WIN: Window = window().unwrap_abort();
+    static ref DOC: Document = WIN.document().unwrap_abort();
     static ref INPUT: HtmlInputElement = q("#input").unchecked_into();
     static ref RECORDS: Node = q("#records").into();
     static ref RECORD: HtmlTemplateElement = q("#record").unchecked_into();
@@ -19,7 +20,7 @@ lazy_static! {
 }
 
 fn q(query: &str) -> Element {
-    DOC.query_selector(query).unwrap().unwrap()
+    DOC.query_selector(query).unwrap_abort().unwrap_abort()
 }
 
 macro_rules! add_listener {
@@ -45,7 +46,7 @@ pub fn init() -> Result<(), JsValue> {
 }
 
 fn add_event_listeners() -> Result<(), JsValue> {
-    add_listener!((DOC.document_element().unwrap(), "click") => |evt| {
+    add_listener!((DOC.document_element().unwrap_abort(), "click") => |evt| {
         let target: Element = match evt.target() {
             Some(target) => target.unchecked_into(),
             None => return Ok(()),
@@ -55,8 +56,8 @@ fn add_event_listeners() -> Result<(), JsValue> {
         }
         let class_name = target.class_name();
         match class_name.as_str() {
-            "inc" => increase_item(target.closest(".record")?.unwrap()),
-            "dec" => decrease_item(target.closest(".record")?.unwrap()),
+            "inc" => increase_item(target.closest(".record")?.unwrap_abort()),
+            "dec" => decrease_item(target.closest(".record")?.unwrap_abort()),
             _ => Ok(()),
         }
     })?;
@@ -87,9 +88,9 @@ fn add_item() -> Result<(), JsValue> {
     let new_record: DocumentFragment = DOC
         .import_node_with_deep(RECORD.content().as_ref(), true)?
         .unchecked_into();
-    let expr_elem = new_record.query_selector(".expr")?.unwrap();
+    let expr_elem = new_record.query_selector(".expr")?.unwrap_abort();
     Node::from(expr_elem).set_text_content(Some(&format!("{}", expr)));
-    let value_elem = new_record.query_selector(".value")?.unwrap();
+    let value_elem = new_record.query_selector(".value")?.unwrap_abort();
     Node::from(value_elem).set_text_content(Some(&format!("{} {}", value, UNIT)));
     RECORDS.insert_before(new_record.as_ref(), RECORDS.first_child().as_ref())?;
     update_total(value);
@@ -105,15 +106,15 @@ fn decrease_item(row: Element) -> Result<(), JsValue> {
 }
 
 fn adjust_item(row: Element, delta: f32) -> Result<(), JsValue> {
-    let expr_elem = Node::from(row.query_selector(".expr")?.unwrap());
-    let expr = expr_elem.text_content().unwrap();
-    let mut expr = Expr::parse(&expr).unwrap();
+    let expr_elem = Node::from(row.query_selector(".expr")?.unwrap_abort());
+    let expr = expr_elem.text_content().unwrap_abort();
+    let mut expr = Expr::parse(&expr).unwrap_abort();
     expr.adjust_factor(delta);
     expr_elem.set_text_content(Some(&format!("{}", expr)));
 
-    let value_elem = Node::from(row.query_selector(".value")?.unwrap());
-    let value = value_elem.text_content().unwrap();
-    let value = Expr::parse(&value).unwrap().calc(UNIT);
+    let value_elem = Node::from(row.query_selector(".value")?.unwrap_abort());
+    let value = value_elem.text_content().unwrap_abort();
+    let value = Expr::parse(&value).unwrap_abort().calc(UNIT);
     update_total(-value);
     let value = expr.calc(UNIT).round();
     if (value - 0.0).abs() < 1e-5 {
@@ -126,8 +127,8 @@ fn adjust_item(row: Element, delta: f32) -> Result<(), JsValue> {
 }
 
 fn update_total(diff: f32) {
-    let current_total = TOTAL.text_content().unwrap();
-    let current_total = Expr::parse(&current_total).unwrap().calc(UNIT);
+    let current_total = TOTAL.text_content().unwrap_abort();
+    let current_total = Expr::parse(&current_total).unwrap_abort().calc(UNIT);
     let new_total = current_total + diff;
     TOTAL.set_text_content(Some(&format!("{} {}", new_total, UNIT)));
 }
